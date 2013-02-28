8aefb583c8eeae5e2f164baaeb81b6aa048e1838
Jared Hanson <jaredhanson@gmail.com>
2013-02-28 08:37:53 -0800

    Test cases for Site.

--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,9 @@ describe('kerouac', function() {
     
   it('should export function', function() {
     expect(kerouac).to.be.a('function');
+    
+    var site = kerouac();
+    expect(site.constructor.name).to.equal('Kerouac');
   });
   
 });
---
+++ test/site.test.js
@@ -0,0 +1,20 @@
+var Site = require('site')
+  , Page = require('page');
+
+
+describe('Site', function() {
+    
+  it('should add pages by path', function() {
+    var site = new Site();
+    expect(Object.keys(site)).to.have.length(0);
+    
+    site.add('/index.html');
+    expect(Object.keys(site)).to.have.length(1);
+    expect(site['/index.html']).to.be.instanceof(Page);
+    
+    site.add('/about.html');
+    expect(Object.keys(site)).to.have.length(2);
+    expect(site['/about.html']).to.be.instanceof(Page);
+  });
+  
+});
