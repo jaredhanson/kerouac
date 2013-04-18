2c39ac1383a752c1849f967542178236677bdf40
Jared Hanson <jaredhanson@gmail.com>
2013-04-18 08:26:58 -0700

    Add test case for Site with path declared twice.

--- test/site.test.js
+++ test/site.test.js
@@ -17,4 +17,18 @@ describe('Site', function() {
     expect(site['/about.html']).to.be.instanceof(Page);
   });
   
+  it('should have single page when adding path twice', function() {
+    var site = new Site();
+    expect(Object.keys(site)).to.have.length(0);
+    
+    site.add('/legal.html');
+    expect(Object.keys(site)).to.have.length(1);
+    var p = site['/legal.html'];
+    expect(p).to.be.instanceof(Page);
+    
+    site.add('/legal.html');
+    expect(Object.keys(site)).to.have.length(1);
+    expect(site['/legal.html']).to.be.equal(p);
+  });
+  
 });
