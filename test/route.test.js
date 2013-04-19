c8dd6220e1368a277648cb0354e5b67000389db7
Jared Hanson <jaredhanson@gmail.com>
2013-04-18 19:03:22 -0700

    Add test case for Route with parameterized path.

--- test/route.test.js
+++ test/route.test.js
@@ -3,7 +3,7 @@ var Route = require('route');
 
 describe('Route', function() {
   
-  describe('with whole path', function() {
+  describe('with path', function() {
     
     var route = new Route('/welcome', [ function(){} ]);
     
@@ -23,6 +23,38 @@ describe('Route', function() {
     it('should match correctly', function() {
       expect(route.match('/welcome')).to.be.true;
       expect(route.match('/not-welcome')).to.be.false;
+    });
+  
+  });
+  
+  describe('with parameterized path', function() {
+    
+    var route = new Route('/blog/:year/:month/:day/:slug', [ function(){} ]);
+    
+    it('should have path property', function() {
+      expect(route.path).to.equal('/blog/:year/:month/:day/:slug');
+    });
+    
+    it('should have fns property', function() {
+      expect(route.fns).to.be.instanceof(Array);
+      expect(route.fns).to.have.length(1);
+    });
+    
+    it('should not have whole path', function() {
+      expect(route.isWholePath()).to.be.false;
+    });
+    
+    it('should match correctly', function() {
+      expect(route.match('/blog/2013/04/18/hello-world')).to.be.true;
+      expect(route.params).to.be.instanceof(Object);
+      expect(Object.keys(route.params)).to.have.length(4);
+      expect(route.params.year).to.equal('2013');
+      expect(route.params.month).to.equal('04');
+      expect(route.params.day).to.equal('18');
+      expect(route.params.slug).to.equal('hello-world');
+      
+      expect(route.match('/blog/2013/04/18')).to.be.false;
+      expect(route.match('/not-blog/2013/04/18/hello-world')).to.be.false;
     });
   
   });
