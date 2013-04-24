3d55d77a57a8cad1df36a9373fd85e920f149aef
Jared Hanson <jaredhanson@gmail.com>
2013-04-24 11:58:16 -0700

    Test case for render middleware.

---
+++ test/middleware/render.test.js
@@ -0,0 +1,18 @@
+var render = require('middleware/render');
+
+
+describe('render middleware', function() {
+  
+  it('should render page', function(done) {
+    var middleware = render();
+    var page = {};
+    page.render = function() {
+      expect(arguments).to.have.length(0);
+      done();
+    }
+    
+    middleware(page, function(err) {
+    });
+  });
+  
+});
