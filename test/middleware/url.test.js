90f65adc7aa68f1b6bb8ca5769dcd28bb98cbcb6
Jared Hanson <jaredhanson@gmail.com>
2013-04-23 17:55:17 -0700

    Test case for URL middleware.

---
+++ test/middleware/url.test.js
@@ -0,0 +1,36 @@
+var url = require('middleware/url');
+
+
+describe('url middleware', function() {
+  
+  it('should set URL of page', function(done) {
+    var middleware = url();
+    var page = {};
+    page.path = 'foo.html';
+    page.site = {
+      get: function(key) { if (key == 'base url') return 'http://www.example.com/'; }
+    }
+    
+    middleware(page, function(err) {
+      if (err) { return done(err); }
+      expect(page.url).to.equal('http://www.example.com/foo.html');
+      done();
+    });
+  });
+  
+  it('should not set URL of page if base url is not defined', function(done) {
+    var middleware = url();
+    var page = {};
+    page.path = 'foo.html';
+    page.site = {
+      get: function(key) { return undefined; }
+    }
+    
+    middleware(page, function(err) {
+      if (err) { return done(err); }
+      expect(page.url).to.be.undefined;
+      done();
+    });
+  });
+  
+});
