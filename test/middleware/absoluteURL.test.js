36d76d568252281aa4a823bc666532c21bd84e58
Jared Hanson <jaredhanson@gmail.com>
2023-09-12 16:24:21 -0700

    Add tests.

--- test/middleware/absoluteURL.test.js
+++ test/middleware/absoluteURL.test.js
@@ -63,6 +63,38 @@ describe('middleware/absoluteURL', function() {
     });
   });
   
+  it('should preserve pretty URL when base URL is not set', function(done) {
+    var middleware = absoluteURL();
+    var page = {};
+    page.app = { get: function() { return 'http://www.example.com/'; } }
+    page.path = '/home.html';
+    page.url = '/home/';
+  
+    middleware(page, function(err) {
+      if (err) { return done(err); }
+      expect(page.path).to.equal('/home.html');
+      expect(page.url).to.equal('/home/');
+      expect(page.fullURL).to.equal('http://www.example.com/home/');
+      done();
+    });
+  });
+  
+  it('should preserve pretty nested URL when base URL is not set', function(done) {
+    var middleware = absoluteURL();
+    var page = {};
+    page.app = { get: function() { return 'http://www.example.com/'; } }
+    page.path = '/2000/01/01/hello.html';
+    page.url = '/2000/01/01/hello/';
+  
+    middleware(page, function(err) {
+      if (err) { return done(err); }
+      expect(page.path).to.equal('/2000/01/01/hello.html');
+      expect(page.url).to.equal('/2000/01/01/hello/');
+      expect(page.fullURL).to.equal('http://www.example.com/2000/01/01/hello/');
+      done();
+    });
+  });
+  
   it('should preserve path when base URL is set to domain', function(done) {
     var middleware = absoluteURL();
     var page = {};
