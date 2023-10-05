16704fe591d30480f19ad6bb281311421567199f
Jared Hanson <jaredhanson@gmail.com>
2023-10-04 18:50:13 -0700

    Add tests.

--- test/page-ex.test.js
+++ test/page-ex.test.js
@@ -18,16 +18,37 @@ describe('Page extensions', function() {
           app: { configurable: true, enumerable: true, writable: true, value: app }
         }));
       
-      page.render('foo', function(err, str) {
+      page.render('index', function(err, str) {
         if (err) { return done(err); }
         expect(str).to.equal('<p>Hello</p>');
-        expect(page.app.render.getCall(0).args[0]).to.equal('foo');
+        expect(page.app.render.getCall(0).args[0]).to.equal('index');
         expect(page.app.render.getCall(0).args[1]).to.deep.equal({
           _locals: undefined
         });
         done();
       });
     }); // should render layout and invoke callback
+    
+    it('should render layout with locals and invoke callback', function(done) {
+      var app = new function(){};
+      app.render = sinon.stub().yieldsAsync(null, '<p>Hello</p>');
+      
+      var page = new Page();
+      setPrototypeOf(page, Object.create(pagex, {
+          app: { configurable: true, enumerable: true, writable: true, value: app }
+        }));
+      
+      page.render('user', { name: 'Tobi' }, function(err, str) {
+        if (err) { return done(err); }
+        expect(str).to.equal('<p>Hello</p>');
+        expect(page.app.render.getCall(0).args[0]).to.equal('user');
+        expect(page.app.render.getCall(0).args[1]).to.deep.equal({
+          user: 'Tobi',
+          _locals: undefined
+        });
+        done();
+      });
+    }); // should render layout with locals and invoke callback
     
   }); // #render
   
