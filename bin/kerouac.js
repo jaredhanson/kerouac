e67ff0569d4374b4d5ebe5d2c0420c93b2ec3b3e
Jared Hanson <jaredhanson@gmail.com>
2023-12-14 19:21:03 -0800

    Initial serve implementation.

--- bin/kerouac.js
+++ bin/kerouac.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const cli = require('../lib/cli');
 
+
+console.log('kerouac... do something!');
+
+yargs(hideBin(process.argv))
+  .command('serve [port]', 'start the server', function(yargs) {
+    return yargs
+      .positional('port', {
+        describe: 'port to bind on',
+        default: 5000
+      });
+  }, cli.serve)
+  .parse()
+
+
+
+/*
 var program = require('commander')
   , kerouac = require('../');
 
@@ -10,3 +29,4 @@ program.command('build')
   });
 
 program.parse(process.argv);
+*/
