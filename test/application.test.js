f39802970c7f004f2dd6fbe2725ad9a7e6216e87
Jared Hanson <jaredhanson@gmail.com>
2024-02-21 16:47:06 -0800

    Clean up tests.

--- test/application.test.js
+++ test/application.test.js
@@ -93,46 +93,6 @@ describe('application', function() {
   
   
   
-  describe('#xfmx', function() {
-    
-    it('should parse YAML front matter', function() {
-      var str = "layout: 'yaml'\n"
-              + "title: 'Hello YAML'\n";
-      
-      var fm = app.fm(str);
-      expect(fm).to.deep.equal({
-        layout: 'yaml',
-        title: 'Hello YAML'
-      });
-    });
-    
-    it('should parse JSON front matter', function() {
-      var str = "{ 'layout': 'json', 'title': 'Hello JSON' }"
-      
-      var fm = app.fm(str);
-      expect(fm).to.deep.equal({
-        layout: 'json',
-        title: 'Hello JSON'
-      });
-    }); // should parse JSON front matter
-    
-    it('should not parse empty front matter', function() {
-      var str = '';
-      
-      var fm = app.fm(str);
-      expect(fm).to.be.undefined;
-    }); // should not parse empty front matter
-    
-    it('should throw when parsing invalid front matter', function() {
-      var str = 'beep boop';
-      
-      expect(function() {
-        app.fm(str);
-      }).to.throw(SyntaxError);
-    }); // should throw when parsing invalid front matter
-    
-  }); // #fm
-  
   describe('#highlight', function() {
     
     it('should highlight syntax', function() {
